66f7bd96fe47c3d6c28c306fd4f7610b2a0f7346
Tomik David <tomik.david1@gmail.com>
2023-12-04 16:10:43 +0100

    kesz beckend

--- src/valuta.ts
+++ src/valuta.ts
@@ -1,8 +1,26 @@
+/**
+ * Valuta interface létrehozása
+ */
 export interface valuta{
+    /**
+     * szám alapú id 
+     */
     id: number;
+    /**
+     * valuta 3 tagú kódjának megadása
+     */
     valuta: string;
+    /**
+     * arfolyam megadása forintba
+     */
     arfolyam: number;
 }
+/**
+ * valuták interface létrehozása
+ */
 export interface valutak{
+    /**
+     * interfacebe valuták tőmbbe való behelyezése
+     */
     valutak: valuta[];
 }
